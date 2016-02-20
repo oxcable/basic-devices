4d06365a49721dfba4915e8492acf9dbbb395915
Michael Nye <thenyeguy@gmail.com>
2016-02-19 22:01:10 -0800

    Adds testing binaries.

---
+++ src/bin/test_reverb.rs
@@ -0,0 +1,25 @@
+//! Applies reverb to microphone input, then plays back out to the speakers.
+
+extern crate oxcable;
+extern crate oxcable_basic_devices;
+
+#[cfg(not(test))]
+fn main() {
+    use oxcable::chain::{DeviceChain, Tick};
+    use oxcable::io::audio::AudioEngine;
+    use oxcable_basic_devices::reverb::{MoorerReverb, rooms};
+
+    println!("Initializing signal chain...");
+    let engine = AudioEngine::with_buffer_size(256).unwrap();
+    let mut chain = DeviceChain::from(
+        engine.default_input(1).unwrap()
+    ).into(
+        MoorerReverb::new(rooms::HALL, 1.0, -3.0, 0.5, 1)
+    ).into(
+        engine.default_output(1).unwrap()
+    );
+
+    println!("Playing... Press Enter to quit.");
+    chain.tick_until_enter();
+    println!("Done!");
+}
