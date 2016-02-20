4c0623e8e41bc7e8a37333a93a648c005945c1db
Michael Nye <thenyeguy@gmail.com>
2016-02-19 22:13:39 -0800

    Write top level module docs.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+//! Basic audio filters for making music with Rust.
+//!
+//! This is an extension to the
+//! [`oxcable` framework](https://github.com/oxcable/oxcable). These devices are
+//! designed to be used with the basic tools provided in the framework. See the
+//! core crate for further details.
+
 extern crate num;
 extern crate oxcable;
 
